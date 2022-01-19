8d96e1202c8d7bcf209b67d7cba8df7f7c19686b
FYH-fly <18237346502@163.com>
2022-01-20 00:04:51 +0800

    add files

---
+++ build.sh
@@ -0,0 +1,11 @@
+
+
+cd build
+
+make clean
+
+cmake ..
+
+make
+
+
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "lib_add/demo_add.h"
+#include <iostream>
+#include <string>
+
+
+int main() {
+    int n = 100;
+    int ans = my_add(n);
+    
+    std::cout << "1+2+...+" << n << "=" << ans << std::endl;
+    
+    
+}
+
